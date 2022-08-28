356a732283828d77b6f3d05e94a01b9357726e8e
Yang Tawu Tawu Aja <112259647+YangTawuTawuAja@users.noreply.github.com>
2022-08-28 10:03:54 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -5,27 +5,27 @@ public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 //input
-        System.out.println("Input");
+        System.out.format("Input%n");
 
-        System.out.print("Nama: ");
+        System.out.format("Nama: ");
         String nama = input.nextLine();
 
-        System.out.print("Umur: ");
+        System.out.format("Umur: ");
         int umur = input.nextInt();
 
-        System.out.print("Berat: ");
+        System.out.format("Berat: ");
         long berat = input.nextLong();
 
-        System.out.print("Tinggi: ");
+        System.out.format("Tinggi: ");
         float tinggi = input.nextFloat();
 
-        System.out.print("Kelas: ");
+        System.out.format("Kelas: ");
         byte kelas = input.nextByte();
 
-        System.out.print("Jumlah Uang: ");
+        System.out.format("Jumlah Uang: ");
         double uang = input.nextDouble();
 
-        System.out.print("Status Menikah: ");
+        System.out.format("Status Menikah: ");
         boolean nikah = input.nextBoolean();
 
 //output
@@ -33,7 +33,7 @@ public class main {
         System.out.format("Nama: %s%n", nama);
         System.out.format("Umur: %d Th%n", umur);
         System.out.format("Berat Badan: %d kg%n", berat);
-        System.out.format("Tinggi: %.2f cm%n", tinggi);
+        System.out.format("Tinggi: %.0f cm%n", tinggi);
         System.out.format("Kelas: %d%n", kelas);
         System.out.format("Duit Kamu: Rp.%,.2f%n", uang);
         System.out.format("Status Menikah: %b%n", nikah);
